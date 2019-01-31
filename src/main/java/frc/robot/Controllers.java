76e5f677d015d9a0af3d7796e9201973bebed393
baxtrax <bradofficialbox@gmail.com>
2019-01-31 00:57:29 -0500

    Adding in Controllers class

---
+++ src/main/java/frc/robot/Controllers.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Controllers {
+
+    public static WPI_TalonSRX LEFT_DRIVE = new WPI_TalonSRX(1);
+    public static WPI_TalonSRX RIGHT_DRIVE = new WPI_TalonSRX(2);
+}
