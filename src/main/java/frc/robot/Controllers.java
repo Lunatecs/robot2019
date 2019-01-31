9fbc6ce7b3a40e7b7db716510862857c715fc162
baxtrax <bradofficialbox@gmail.com>
2019-01-31 01:20:06 -0500

    Added init method and filled in left and right drive motor controllers

--- src/main/java/frc/robot/Controllers.java
+++ src/main/java/frc/robot/Controllers.java
@@ -1,9 +1,19 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class Controllers {
 
-    public static WPI_TalonSRX LEFT_DRIVE = new WPI_TalonSRX(1);
-    public static WPI_TalonSRX RIGHT_DRIVE = new WPI_TalonSRX(2);
+    public static WPI_TalonSRX LEFT_DRIVE1_T = new WPI_TalonSRX(16);
+    public static WPI_VictorSPX LEFT_DRIVE2_V = new WPI_VictorSPX(14);
+    public static WPI_VictorSPX LEFT_DRIVE3_V = new WPI_VictorSPX(15);
+
+    public static WPI_TalonSRX RIGHT_DRIVE1_T = new WPI_TalonSRX(2);
+    public static WPI_VictorSPX RIGHT_DRIVE2_V = new WPI_VictorSPX(2);
+    public static WPI_VictorSPX RIGHT_DRIVE3_V = new WPI_VictorSPX(2);
+
+    public static void init() {
+
+    }
 }
